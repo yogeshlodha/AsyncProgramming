3e6c263dde23ae872ee7f9c07ab2dffd137eac68
yogesh lodha <yolodha@gmail.com>
2021-09-24 09:54:52 +0530

    Add project files.

---
+++ AsyncProgramming/Controllers/ThreadTaskController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsyncProgramming.Controllers
+{
+    public class ThreadTaskController : Controller
+    {
+        /// <summary>
+        /// Sleep method, process wait for 8 second. 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> ASleepMethod()
+        {
+            // 8 Second sleep.
+            await Task.Delay(8000);
+            return true;
+        }
+
+        /// <summary>
+        /// Async process wait for responce.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ActionResult> AsyncLoad()
+        {
+          var tBool=  await ASleepMethod();
+            return View();
+        }
+
+        /// <summary>
+        /// Sync process do not wait for sleep responce.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SyncLoad()
+        {
+            var tBool = ASleepMethod();
+            return View();
+        }
+    }
+}
